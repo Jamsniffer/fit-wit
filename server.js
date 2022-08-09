8277619a0f5ce3242579016470f050a1d2375111
kodapassey <kodapassey@charter.net>
2022-08-08 21:03:01 -0700

    current workout js all added

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const path = require('path');
 const express = require('express');
-const dotenv = require ('dotenv');
-const mysql = require ('mysql2');
+const dotenv = require('dotenv');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql2');
+const helpers = require('./utils/helpers');
+
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
